a105e90fe9f4be6f14f38b5589fe6971c4c501d8
automvc <honeysoft@126.com>
2025-01-05 19:52:05 +0800

    add batch insert example

--- test/exam/BatchInsertTest2.py
+++ test/exam/BatchInsertTest2.py
@@ -1,35 +1,19 @@
-from bee.api import Suid, SuidRich
-from bee.sqllib import BeeSql
+from bee.api import SuidRich
 from test.entity.Orders import Orders
-from test.entity.Student import Student
 
-
-# from test.entity.Student import Student
+""" batch insert for orders """
 if __name__ == '__main__':
     print("start")
     
-    createSql = """
-    CREATE TABLE users (  
-    id INTEGER PRIMARY KEY NOT NULL, 
-    name VARCHAR(100),  
-    age INT,  
-    remark VARCHAR(255),  
-    addr VARCHAR(255)  
-    );  
-    """
+    orders0=Orders()
+    orders0.name = "bee"
     
-    # beeSql=BeeSql()
-    # beeSql.modify(createSql, []);
-    
-    student0=Orders()
-    student0.name = "bee"
-    
-    student1=Orders()
-    student1.name = "bee1"
+    orders1=Orders()
+    orders1.name = "bee1"
     
     entity_list=[]
-    entity_list.append(student0)
-    entity_list.append(student1)
+    entity_list.append(orders0)
+    entity_list.append(orders1)
     
     suidRich = SuidRich()
     insertNum = suidRich.insert_batch(entity_list)
