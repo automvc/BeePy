78c965a029db1844d9904771af6aa8af969380da
automvc <honeysoft@126.com>
2025-08-23 00:00:24 +0800

    enhance the code

--- src/bee/factory.py
+++ src/bee/factory.py
@@ -7,48 +7,52 @@ class BeeFactory:
     """
     Bee Factory.
     """
-    
+
     __connection = None
-    
+
     __instance = None
     __honeyFactory = None
-    
+
     def __new__(cls):
-        if cls.__instance is None: 
+        if cls.__instance is None:
             cls.__instance = super().__new__(cls)
-        return cls.__instance 
-        
+        return cls.__instance
+
     def set_connection(self, connection):
+        '''
+        set connection
+        '''
         BeeFactory.__connection = connection
-    
+
     def get_connection(self):
+        '''
+        get connection
+        '''
         return BeeFactory.__connection
-    
-    __nameTranslate = None
-    
+
+    def __init__(self):
+        self.__nameTranslate = None
+
     def getInitNameTranslate(self) -> NameTranslate:
+        '''
         #     (DB<-->Python),
-        # 1: order_no<-->orderNo 
+        # 1: order_no<-->orderNo
         # 2: ORDER_NO<-->orderNo
         # 3: original,
         # 4: ORDER_NO<-->order_no (DbUpperAndPythonLower)
+        '''
         if self.__nameTranslate is None:
             translateType = HoneyConfig.naming_translate_type
-            if translateType == 1: __nameTranslate = UnderScoreAndCamelName()
-            elif translateType == 2: __nameTranslate = UpperUnderScoreAndCamelName()
-            elif translateType == 3: __nameTranslate = OriginalName()
-            elif translateType == 4: __nameTranslate = DbUpperAndPythonLower()
+            if translateType == 1:
+                self.__nameTranslate = UnderScoreAndCamelName()
+            elif translateType == 2:
+                self.__nameTranslate = UpperUnderScoreAndCamelName()
+            elif translateType == 3:
+                self.__nameTranslate = OriginalName()
+            elif translateType == 4:
+                self.__nameTranslate = DbUpperAndPythonLower()
             # else:__nameTranslate = UnderScoreAndCamelName()
-            else:__nameTranslate = OriginalName()  # v1.6.2
-                
-        return __nameTranslate
-    
-    # def __getattribute__(self, item):  
-    #     print(f"Accessing attribute: {item}") 
-    #     return super().__getattribute__(item)
-    
-    # def getHoneyFactory(self):
-    #     if self.__honeyFactory is None:
-    #         __honeyFactory = HoneyFactory()
-    #     return __honeyFactory
-    
+            else:
+                self.__nameTranslate = OriginalName()  # v1.6.2
+
+        return self.__nameTranslate
